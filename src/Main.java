703428dcd7ee77bc018178aaaf23421e0f8ef059
spicysalami <108631028+spicysalami@users.noreply.github.com>
2023-06-19 12:08:11 +0200

    esercizio 2

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,57 @@
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        Student mario = new Student("Mario");
-        Student luigi = new Student("Luigi");
+        HashMap<String, Product> Inventory = new HashMap<>();
 
-        mario.addGrade(30);
-        mario.addGrade(18);
-        mario.addGrade(27);
-        mario.addGrade(29);
+        // Creazione prodotti
+        Product product1 = new Product("v8H7v", "Mela", 0.30);
+        Product product2 = new Product("3oHZD", "Pera", 1.00);
+        Product product3 = new Product("Fx4ZE", "Banana", 0.50);
+        Product product4 = new Product("vd8NL", "Anguria", 1.40);
+        Product product5 = new Product("dxAE4", "Kiwi", 1.10);
+        Product product6 = new Product("3qBfT", "Melone", 0.80);
 
-        luigi.addGrade(26);
-        luigi.addGrade(28);
-        luigi.addGrade(29);
-        luigi.addGrade(29);
-        luigi.addGrade(30);
 
-        // confronto medie
-        float mean1 = mario.GetAverageGrade();
-        float mean2 = luigi.GetAverageGrade();
+        // Aggiunta prodotti
+        Inventory.put(product1.getId(),product1);
+        Inventory.put(product2.getId(),product2);
+        Inventory.put(product3.getId(),product3);
+        Inventory.put(product4.getId(),product4);
+        Inventory.put(product5.getId(),product5);
+        Inventory.put(product6.getId(),product6);
 
-        if (mean1 > mean2) {
-            System.out.println("Mario ha la media più alta: " + mean1);
-        } else System.out.println("Luigi ha la media più alta: " + mean2);
+        System.out.println(Inventory.size()); //stampo quantitò
+
+        //ricerca tramite id
+        String idToFind = "vd8NL";
+        if (Inventory.containsKey(idToFind)) {
+            Product product = Inventory.get(idToFind);
+            System.out.println(product);
+        } else  System.out.println("Prodotto non trovato");
+
+        //eliminazione tramite id
+        String idToRemove = "3oHZD";
+        if (Inventory.containsKey(idToRemove)) {
+            Inventory.remove(idToRemove);
+            System.out.println(Inventory); //Dimostrazione
+        } else  System.out.println("Prodotto non trovato");
+
+        //modifica tramite id
+        String idSearched = "Fx4ZE";
+        Double newPrice = 9.99;
+        if (Inventory.containsKey(idSearched)) {
+            Product EditedProduct = Inventory.get(idSearched);
+            EditedProduct.setPrice(newPrice);
+            System.out.println(EditedProduct); //Dimostrazione
+        } else  System.out.println("Prodotto non trovato");
+
+
+        //somma intero valore inventario
+        double totalValue = 0.0;
+        for (Product product : Inventory.values()) {
+            totalValue += product.getPrice();
+        }
+        System.out.println("Valore totale dell'inventario: " + totalValue);
     }
 }
---
+++ src/Product.java
@@ -0,0 +1,36 @@
+public class Product {
+    String id;
+    String name;
+    Double price;
+
+    public Product(String id, String name, Double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
