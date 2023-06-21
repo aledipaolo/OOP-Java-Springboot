ea4b95a920fe6f9fa6f5abe541f4d238f9bafc73
spicysalami <108631028+spicysalami@users.noreply.github.com>
2023-06-21 09:50:51 +0200

    demonstrated use of public, private and protected.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import models.Car;
+
 public class Main {
     public static void main(String[] args) {
+        Car car = new Car ("Fiesta", "Ford", 2004);
 
+        System.out.println(car.year);
+        /*
+        System.out.println(car.model); // Error: manufacturer is private
+        System.out.println(car.manufacturer); // Error: manufacturer is protected
+         */
+
+        // Can get them from methods
+        System.out.println(car.getModel());
+        System.out.println(car.getManufacturer());
     }
 }
